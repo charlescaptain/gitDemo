311b49d93ebb6d8568af03a74b0873a6ce879d03
eclipse_user <eclipse_user@aowin.com>
2020-04-13 06:44:37 +0800

    my first commit in eclipse

---
+++ src/main/java/com/aowin/git/Apple.java
@@ -0,0 +1,8 @@
+package com.aowin.git;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/aowin/test/GoodTest.java
@@ -0,0 +1,8 @@
+package com.aowin.test;
+
+public class GoodTest {
+
+	public static void main(String[] args) {
+		
+	}
+}
